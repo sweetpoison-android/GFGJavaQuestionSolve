a49584315137ba9ce6dc364ab230eed9627be5b0
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-14 13:24:03 +0530

    swap two number with the help of third variable

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,6 +4,20 @@ public class MyClass {
 
     public static void main(String[] args)
     {
-        System.out.println("hello");
+     swapNumberByThirdVariable(1, 5);
+    }
+
+   public static void swapNumberByThirdVariable(int a, int b)
+    {
+        int thirdNumber =0;
+        thirdNumber = a;
+
+        a=b;
+
+        b= thirdNumber;
+
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+
     }
 }
