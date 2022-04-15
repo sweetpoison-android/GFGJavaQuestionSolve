950159b555263d0ccc6131bd8ceab7afe51e76c4
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-15 15:55:42 +0530

    find armstrong number from given range

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -2,41 +2,46 @@ package com.example.gfgjavaquestion;
 
 public class MyClass {
 
-    public static void main(String[] args)
-    {
-        findPrimeNumberFromGivenRange(1, 100);
+    public static void main(String[] args) {
+
+        findArmStrongNumber(50, 1000);
+        System.out.println();
 
     }
 
-   public static void findPrimeNumberFromGivenRange(int a, int b)
-    {
-        int isDivided, count = 0;
+    public static void findArmStrongNumber(int a, int b) {
 
-        for (int i=a; i<=b ; i++)
-        {
-            if (i == 0 || i==1)
-                continue;
+        for (int j = a + 1; j < b; ++j) {
 
-            isDivided = 1;
-            for (int j=2; j<=i/2; j++)
-            {
-                if (i%j == 0)
-                {
-                    isDivided = 0;
-                    break;
-                }
+            // Calculating number of digits
 
+            int y = j;
+            int N = 0;
+            while (y != 0) {
+                y /= 10;
+                ++N;
+            }
+            // Calculating the sum of nth
+            // power of all the digits
+
+            int sum_power = 0;
+            y = j;
+            while (y != 0) {
+                int d = y % 10;
+                sum_power += Math.pow(d, N);
+                y /= 10;
             }
 
-            if (isDivided == 1)
-            {
-                System.out.println(i);
-               count++;
+            // Checking if the current number
+            // i is equal to the sum of nth
+            // power of all the digits
 
-            }
+            if (sum_power == j)
+                System.out.print(j + " ");
 
         }
-        System.out.printf("Total prime number"+count);
-        }
+
+
+
     }
-
+}
