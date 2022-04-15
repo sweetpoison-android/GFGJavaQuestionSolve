361cdac9008972c8f9ebaf01365340991b2f1355
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-15 16:27:53 +0530

    neon number checker

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,44 +4,32 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        findArmStrongNumber(50, 1000);
+        checkNeonNumber(9);
         System.out.println();
 
     }
 
-    public static void findArmStrongNumber(int a, int b) {
+    public static void checkNeonNumber(int a) {
 
-        for (int j = a + 1; j < b; ++j) {
+        int sum=0;
+        int square =0;
+        square = a*a;
 
-            // Calculating number of digits
-
-            int y = j;
-            int N = 0;
-            while (y != 0) {
-                y /= 10;
-                ++N;
-            }
-            // Calculating the sum of nth
-            // power of all the digits
-
-            int sum_power = 0;
-            y = j;
-            while (y != 0) {
-                int d = y % 10;
-                sum_power += Math.pow(d, N);
-                y /= 10;
-            }
-
-            // Checking if the current number
-            // i is equal to the sum of nth
-            // power of all the digits
-
-            if (sum_power == j)
-                System.out.print(j + " ");
-
+        while (square>0)
+        {
+            int result = square%10;
+            sum +=result;
+            square = square/10;
         }
 
-
+        if (sum == a)
+        {
+            System.out.println("number is neon");
+        }
+        else
+        {
+            System.out.println("number is not neon");
+        }
 
     }
 }
