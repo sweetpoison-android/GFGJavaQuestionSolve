f84ef9f0c9b22a5b877fa9b05003b7c556d107e5
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-16 14:51:29 +0530

    pyramid triangle

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,26 +4,24 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        printLeftTriangle(5);
+        pyramidStarPattern(5);
 
     }
 
-    public static void printLeftTriangle(int n) {
+    public static void pyramidStarPattern(int n) {
 
-     for (int i=0; i<n; i++)
-     {
-
-         for (int j=2*(n-i); j>=0; j-- )
-         {
-             System.out.print(" ");
-         }
-
+    for (int i=0; i<n; i++)
+    {
+        for (int j = n-i; j>1; j--)
+        {
+            System.out.print(" ");
+        }
         for (int j=0; j<=i; j++)
         {
             System.out.print("* ");
         }
-         System.out.println();
-     }
+        System.out.println();
+    }
 
     }
 }
--- app/src/main/java/com/example/gfgjavaquestionsolve/MainActivity.java
+++ app/src/main/java/com/example/gfgjavaquestionsolve/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.gfgjavaquestionsolve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(view ->
+        {
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        });
     }
 }
