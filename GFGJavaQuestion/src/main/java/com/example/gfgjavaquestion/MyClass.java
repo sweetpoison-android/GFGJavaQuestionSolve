0fcd7e39a134aa75d3df5008feb1cd5563d163be
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-16 10:52:59 +0530

    print right triangle

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,16 +4,20 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-       double SI = findCompoundInterest(5765, 5, 2);
-        System.out.println("Simple interest : "+SI);
+      printRightTriangle(5);
 
     }
 
-    public static double findCompoundInterest(double p, double r, double t) {
+    public static void printRightTriangle(int n) {
 
-      double compoundInterest = p*(Math.pow((1+r/100), t));
-
-       return compoundInterest;
+     for (int i=0; i<=n; i++)
+     {
+        for (int j=0; j<=i; j++)
+        {
+            System.out.print(" *");
+        }
+         System.out.println(" ");
+     }
 
     }
 }
