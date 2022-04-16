7372a35d11177268ca5fa5e36c1f6e4c071f2164
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-16 11:13:37 +0530

    print left triangle

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,19 +4,25 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-      printRightTriangle(5);
+        printLeftTriangle(5);
 
     }
 
-    public static void printRightTriangle(int n) {
+    public static void printLeftTriangle(int n) {
 
-     for (int i=0; i<=n; i++)
+     for (int i=0; i<n; i++)
      {
+
+         for (int j=2*(n-i); j>=0; j-- )
+         {
+             System.out.print(" ");
+         }
+
         for (int j=0; j<=i; j++)
         {
-            System.out.print(" *");
+            System.out.print("* ");
         }
-         System.out.println(" ");
+         System.out.println();
      }
 
     }
