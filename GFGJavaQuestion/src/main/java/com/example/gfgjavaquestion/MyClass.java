3832cd3f70904b1bdbe6f8becd407cc32d5a31af
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-18 18:05:53 +0530

    mirror Upper Star Triangle Pattern

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,30 +4,67 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        reversePyramidStarPattern(5);
+        mirrorUpperStarTrianglePattern(5);
 
     }
 
-    public static void reversePyramidStarPattern(int number) {
+    public static void mirrorUpperStarTrianglePattern(int number) {
 
-        for(int i = number; i >= 1; i--)
-        {
+   upperPartOfPyramidTriangle(5);
+   lowerPartOfPyramidTriangle(3);
 
-            // Inner loop print space
-            for(int j = i; j < number; j++)
-            {
-                System.out.print("  ");
+    }
+
+    public static void upperPartOfPyramidTriangle(int number)
+    {
+        int m, n;
+
+        for (m = number - 1; m >= 0; m--) {
+
+            // Inner loop 1
+            for (n = 0; n < m; n++) {
+
+                // Printing whitespace
+                System.out.print(" ");
             }
 
-            // Inner loop print star
-            for(int j = 1; j <= (2 * i - 1); j++)
-            {
-                System.out.print(" *");
+            // Inner loop 2
+            for (n = m; n <= number - 1; n++) {
+
+                // Printing star with whitespace
+                System.out.print("*"
+                        + " ");
             }
 
-            // Ending line after each row
-            System.out.println("");
+            // By now we are done with one row so new line
+            System.out.println();
         }
+    }
 
+    public static void lowerPartOfPyramidTriangle(int number)
+    {
+        int m, n;
+
+        // Outer loop for rows
+        for (m = 1; m <= number; m++) {
+
+            // Inner loop 1
+            for (n = 1; n < m; n++) {
+
+                // Printing whitespace
+                System.out.print(" ");
+            }
+
+            // Inner loop 2
+            for (n = m; n <= number; n++) {
+
+                // Printing star and whitespace
+                System.out.print("*"
+                        + " ");
+            }
+
+            // By now we are done with one row so new line
+            System.out.println();
+        }
     }
 }
