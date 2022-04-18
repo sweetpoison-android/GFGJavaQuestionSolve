66587bf500feb85a4b3c9f3b93a09daa201af4c6
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-18 17:50:23 +0530

    reverse pyramid triangle

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -4,24 +4,30 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        pyramidStarPattern(5);
+        reversePyramidStarPattern(5);
 
     }
 
-    public static void pyramidStarPattern(int n) {
+    public static void reversePyramidStarPattern(int number) {
 
-    for (int i=0; i<n; i++)
-    {
-        for (int j = n-i; j>1; j--)
+        for(int i = number; i >= 1; i--)
         {
-            System.out.print(" ");
+
+            // Inner loop print space
+            for(int j = i; j < number; j++)
+            {
+                System.out.print("  ");
+            }
+
+            // Inner loop print star
+            for(int j = 1; j <= (2 * i - 1); j++)
+            {
+                System.out.print(" *");
+            }
+
+            // Ending line after each row
+            System.out.println("");
         }
-        for (int j=0; j<=i; j++)
-        {
-            System.out.print("* ");
-        }
-        System.out.println();
-    }
 
     }
 }
