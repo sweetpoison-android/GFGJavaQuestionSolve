69139b4fb50df1cb334c71518ef82e9536783129
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-22 15:21:30 +0530

    find second highest value from array

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -1,29 +1,45 @@
 package com.example.gfgjavaquestion;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class MyClass {
 
     public static void main(String[] args) {
 
-      Integer[] array = new Integer[]{54, 98, 23, 56, 38, 23, 19, 50, 32, 54};
+      Integer[] array = new Integer[]{54, 98, 23, 56, 38, 23, 19, 50, 32, 54, 23, 98};
 
-        Arrays.sort(array);  // for ascending order sorting
-        System.out.println(Arrays.toString(array));
+        Arrays.sort(array, Collections.<Integer>reverseOrder());  // for ascending order sorting
+        System.out.println("sorting an array : "+Arrays.toString(array));
 
-//        Arrays.sort(array, 2, 5);  // for range sorting last index not count
-//        System.out.println(Arrays.toString(array));
+        ArrayList<Integer> arrayList = new ArrayList<>();
+       Collections.addAll(arrayList, array);
 
-//        Arrays.sort(array, Collections.reverseOrder());
-//        System.out.println(Arrays.toString(array));
+        HashSet<Integer> hashSet = new HashSet<Integer>(arrayList);
 
-//        for (int i : array)
-//        {
-//            System.out.print(i+" , ");
-//        }
+        System.out.println("remove duplicate : "+hashSet);
+
+        TreeSet<Integer> treeSet = new TreeSet<Integer>(hashSet);
+
+        System.out.println("sorting arraylist after removed duplicate "+treeSet);
+
+        int i=0;
+
+        int[] arr = new int[treeSet.size()];
+        for ( int ese : treeSet)
+        {
+            arr[i++] = ese;
+
+        }
+        System.out.println("Second highest value of array : "+arr[arr.length-2]);
 
     }
+
 
 
 }
