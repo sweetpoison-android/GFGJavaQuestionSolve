a8bf55d2354c14d35042e8b9c13f825b1f29cb50
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-26 13:29:55 +0530

    Classes and Objects in Java

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -1,19 +1,70 @@
 package com.example.gfgjavaquestion;
 
+import java.awt.print.Book;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class MyClass {
 
     public static void main(String[] args) {
 
-       double SI = findCompoundInterest(5765, 5, 2);
-        System.out.println("Simple interest : "+SI);
+        MyInfo myInfo = new MyInfo("santosh", "Android developer", "Bihar");
+
+        System.out.println("From Constructor");
+
+        System.out.println("My name is "+myInfo.getName());
+        System.out.println("Mt profession is "+myInfo.getProfession());
+        System.out.println("I am from "+myInfo.getAddress());
+
+        myInfo.setName("Manish Malhotra");
+        myInfo.setProfession(".net Developer");
+        myInfo.setAddress("Delhi");
+
+        System.out.println("From getter setter method");
+
+        System.out.println("My name is "+myInfo.getName());
+        System.out.println("Mt profession is "+myInfo.getProfession());
+        System.out.println("I am from "+myInfo.getAddress());
+
 
     }
+ }
 
-    public static double findCompoundInterest(double p, double r, double t) {
+ class MyInfo
+ {
+     String name, profession, address;
+     MyInfo(String sName, String strProfession, String strAddress)
+     {
+      this.name = sName;
+      this.profession = strProfession;
+      this.address = strAddress;
+     }
 
-      double compoundInterest = p*(Math.pow((1+r/100), t));
+     public String getName()
+     {
+         return name;
+     }
+     public void setName(String strName)
+     {
+         this.name = strName;
+     }
+     public String getProfession()
+     {
+         return profession;
+     }
+     public void setProfession(String strProfession)
+     {
+        this.profession = strProfession;
+     }
 
-       return compoundInterest;
+     public String getAddress()
+     {
+         return address;
+     }
+     public void setAddress(String strAddress)
+     {
+         this.address = strAddress;
+     }
+ }
 
-    }
-}
