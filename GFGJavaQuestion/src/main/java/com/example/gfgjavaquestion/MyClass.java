ff8072b1a85390057873b198bcffeaeabbb95da3
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-27 14:21:24 +0530

    search value from array using linear search

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -2,69 +2,49 @@ package com.example.gfgjavaquestion;
 
 import java.awt.print.Book;
 import java.util.Comparator;
+import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeSet;
+import java.util.function.BiConsumer;
 
 public class MyClass {
 
+
+
     public static void main(String[] args) {
 
-        MyInfo myInfo = new MyInfo("santosh", "Android developer", "Bihar");
+        int arr[] = { 32, 65, 2, 6, 89, 67, 45, 54, 78, 34, 53, 12, 76, 49};
 
-        System.out.println("From Constructor");
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("My name is "+myInfo.getName());
-        System.out.println("Mt profession is "+myInfo.getProfession());
-        System.out.println("I am from "+myInfo.getAddress());
-
-        myInfo.setName("Manish Malhotra");
-        myInfo.setProfession(".net Developer");
-        myInfo.setAddress("Delhi");
-
-        System.out.println("From getter setter method");
-
-        System.out.println("My name is "+myInfo.getName());
-        System.out.println("Mt profession is "+myInfo.getProfession());
-        System.out.println("I am from "+myInfo.getAddress());
+        int index = linearSearch(arr, arr.length, 27);
+        if (index == -1)
+        {
+            System.out.println("number not found");
+        }
+        else
+        {
+            System.out.println("number found at position : " + index);
+        }
 
 
     }
+
+    public static int linearSearch(int arr[], int length, int searchNumber)
+    {
+        for (int i=0; i<length; i++)
+        {
+           if (arr[i] == searchNumber)
+           {
+               return i;
+           }
+        }
+        return -1;
+    }
+
  }
 
- class MyInfo
- {
-     String name, profession, address;
-     MyInfo(String sName, String strProfession, String strAddress)
-     {
-      this.name = sName;
-      this.profession = strProfession;
-      this.address = strAddress;
-     }
 
-     public String getName()
-     {
-         return name;
-     }
-     public void setName(String strName)
-     {
-         this.name = strName;
-     }
-     public String getProfession()
-     {
-         return profession;
-     }
-     public void setProfession(String strProfession)
-     {
-        this.profession = strProfession;
-     }
-
-     public String getAddress()
-     {
-         return address;
-     }
-     public void setAddress(String strAddress)
-     {
-         this.address = strAddress;
-     }
- }
 
