c3c41546c6b2b4838dfdcf9ea72129412531ac62
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-29 10:30:26 +0530

    get factorial of any number

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -15,33 +15,14 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        int arr[] = { 32, 65, 2, 6, 89, 67, 45, 54, 78, 34, 53, 12, 76, 49};
+        int factorial = 1;
 
-        Scanner sc = new Scanner(System.in);
-
-        int index = linearSearch(arr, arr.length, 27);
-        if (index == -1)
-        {
-            System.out.println("number not found");
-        }
-        else
-        {
-            System.out.println("number found at position : " + index);
-        }
-
-
-    }
-
-    public static int linearSearch(int arr[], int length, int searchNumber)
-    {
-        for (int i=0; i<length; i++)
-        {
-           if (arr[i] == searchNumber)
+           for (int i=2; i<=5; i++)
            {
-               return i;
+               factorial *= i;
            }
-        }
-        return -1;
+        System.out.println(factorial);
+
     }
 
  }
