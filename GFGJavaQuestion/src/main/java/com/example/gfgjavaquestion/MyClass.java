aec30d21ba40605bdd6404843327e9481f6c3212
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-29 10:54:41 +0530

    return multiple value by array

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -1,22 +1,32 @@
 package com.example.gfgjavaquestion;
 
-
 public class MyClass {
 
     public static void main(String[] args) {
 
-        System.out.println(getFactorial(6));
+      int ans[] =  returnMultipleValue(100, 16);
+        System.out.println("Addition : " +ans[0]);
+        System.out.println("Subtraction : " +ans[1]);
+        System.out.println("Multiplication : " +ans[2]);
+        System.out.println("Division : " +ans[3]);
+        System.out.println("Remainder : " +ans[4]);
+
+
 
     }
 
-    static int getFactorial(int a)
-    {
-        if (a == 0 || a == 1)
-        {
-            return 1;
-        }
-        return a*getFactorial(a-1);
-    }
+   static int[] returnMultipleValue(int a, int b)
+   {
+      int result[] = new int[5];
+      result[0] = a+b;
+      result[1] = a-b;
+      result[2] = a*b;
+      result[3] = a/b;
+      result[4] = a%b;
+
+      return result;
+   }
+
  }
 
 
