9be752edc2c6d998c4e5c535376add2cac15c787
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-04-29 10:33:22 +0530

    get factorial of any number by recursion

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -1,30 +1,22 @@
 package com.example.gfgjavaquestion;
 
-import java.awt.print.Book;
-import java.util.Comparator;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeSet;
-import java.util.function.BiConsumer;
 
 public class MyClass {
 
-
-
     public static void main(String[] args) {
 
-        int factorial = 1;
-
-           for (int i=2; i<=5; i++)
-           {
-               factorial *= i;
-           }
-        System.out.println(factorial);
+        System.out.println(getFactorial(6));
 
     }
 
+    static int getFactorial(int a)
+    {
+        if (a == 0 || a == 1)
+        {
+            return 1;
+        }
+        return a*getFactorial(a-1);
+    }
  }
 
 
