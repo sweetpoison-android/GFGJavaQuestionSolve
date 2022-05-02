bd3f62127742012ebe561a8cf0519d3396aa19bb
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-05-02 15:16:58 +0530

    palindrome checker

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -6,70 +6,35 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        Account account = new Account(1000);
-        Customer c1 = new Customer(account, "santosh"), c2 = new Customer(account, "Sudhir");
+        Scanner sc = new Scanner(System.in);
 
-       new Thread(c1).start();
-       new Thread(c2).start();
+        long sum = 0,  r, temp;
+
+        System.out.println("Enter number for palindrome check");
+     long a = sc.nextLong();
+
+     temp = a;
+
+   while (a>0)
+   {
+       r = a%10;
+       sum = (sum*10)+r;
+       a = a/10;
+   }
+
+        System.out.println(sum);  // reversed integer
+
+   if (sum == temp)
+   {
+       System.out.println(temp + " is a palindrome number");
+   }
+   else
+   {
+       System.out.println(a+ " is not a palindrome number");
+   }
 
     }
 
- }
-
- class Account
- {
-     int bal;
-
-     Account(int balance)
-     {
-         this.bal = balance;
-
-     }
-
-     public boolean isSufficientBalance(int withdrawal)
-     {
-         if (bal > withdrawal)
-             return true;
-         else return false;
-     }
-
-     public void withdraw(int amount)
-     {
-         bal -= amount;
-         System.out.println("your withdrawal amount is "+ amount);
-         System.out.println("your current balance is " +bal);
-     }
-
- }
-
- class Customer implements Runnable
- {
-     Account account;
-     String name;
-
-     Customer(Account clientAccount, String clientName)
-     {
-         this.account = clientAccount;
-         this.name = clientName;
-     }
-
-     @Override
-     public void run() {
-         Scanner sc = new Scanner(System.in);
-
-         synchronized (account)
-         {
-             System.out.println(name + ", Enter amount for withdrawal");
-             int amount = sc.nextInt();
-
-             if (account.isSufficientBalance(amount))
-             {
-                 account.withdraw(amount);
-             }
-             else System.out.println("Insufficient balance");
-         }
-
-     }
  }
 
 
