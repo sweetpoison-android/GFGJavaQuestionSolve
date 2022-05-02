30908f6bf5e1f411b686d4cf72d65769599044f1
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-05-02 17:58:59 +0530

    reverse an array

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -1,40 +1,45 @@
 package com.example.gfgjavaquestion;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class MyClass {
 
-    public static void main(String[] args) {
+    /* Driver program to test above function */
+    public static void main(String args[]) {
 
-        Scanner sc = new Scanner(System.in);
-
-        long sum = 0,  r, temp;
-
-        System.out.println("Enter number for palindrome check");
-     long a = sc.nextLong();
-
-     temp = a;
-
-   while (a>0)
-   {
-       r = a%10;
-       sum = (sum*10)+r;
-       a = a/10;
-   }
-
-        System.out.println(sum);  // reversed integer
-
-   if (sum == temp)
-   {
-       System.out.println(temp + " is a palindrome number");
-   }
-   else
-   {
-       System.out.println(a+ " is not a palindrome number");
-   }
+        int arr[] = {100,33,88, 56, 44, 90, 110, 33, 22, 66};
+        printArray(arr, arr.length);
+        System.out.println("");
+        reverseArray(arr, 0, arr.length-1);
+        printArray(arr, arr.length);
 
     }
 
+    public static void reverseArray(int arr[], int start, int end)
+    {
+        int temp;
+        while (start < end)
+        {
+           temp = arr[start];
+           arr[start] = arr[end];
+           arr[end] = temp;
+           start++;
+           end--;
+        }
+    }
+    public static void printArray(int arr[], int length)
+    {
+        for (int i =0; i<length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
  }
+
+
 
 
