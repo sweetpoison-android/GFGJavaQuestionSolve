a452d22d3f17e78ff04b9788313b5c04c875cc6e
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-05-02 09:50:02 +0530

    use of synchronized for use multiple thread one by one

--- GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
+++ GFGJavaQuestion/src/main/java/com/example/gfgjavaquestion/MyClass.java
@@ -6,41 +6,70 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number");
-        int a = sc.nextInt();
-        System.out.println("Enter second number");
-        int b = sc.nextInt();
+        Account account = new Account(1000);
+        Customer c1 = new Customer(account, "santosh"), c2 = new Customer(account, "Sudhir");
 
-        GetResult ans = GetResult.getAns(a, b);
-        System.out.println("Addition : "+ ans.sum);
-        System.out.println("Subtraction : "+ ans.sub);
-        System.out.println("Multiplication : "+ ans.mul);
-        System.out.println("Division : "+ ans.div);
-        System.out.println("Remainder : "+ ans.remain);
+       new Thread(c1).start();
+       new Thread(c2).start();
 
     }
 
  }
 
-  class GetResult
-  {
-      int sum,sub,mul,div,remain;
+ class Account
+ {
+     int bal;
 
-      GetResult(int addition, int subtraction,int multiply, int divide, int remainder)
-      {
-          this.sum = addition;
-          this.sub = subtraction;
-          this.mul = multiply;
-          this.div = divide;
-          this.remain = remainder;
-      }
+     Account(int balance)
+     {
+         this.bal = balance;
 
-      public static GetResult getAns(int a, int b)
-      {
-          return new GetResult(a+b, a-b, a*b, a/b, a%b);
-      }
-  }
+     }
 
+     public boolean isSufficientBalance(int withdrawal)
+     {
+         if (bal > withdrawal)
+             return true;
+         else return false;
+     }
+
+     public void withdraw(int amount)
+     {
+         bal -= amount;
+         System.out.println("your withdrawal amount is "+ amount);
+         System.out.println("your current balance is " +bal);
+     }
+
+ }
+
+ class Customer implements Runnable
+ {
+     Account account;
+     String name;
+
+     Customer(Account clientAccount, String clientName)
+     {
+         this.account = clientAccount;
+         this.name = clientName;
+     }
+
+     @Override
+     public void run() {
+         Scanner sc = new Scanner(System.in);
+
+         synchronized (account)
+         {
+             System.out.println(name + ", Enter amount for withdrawal");
+             int amount = sc.nextInt();
+
+             if (account.isSufficientBalance(amount))
+             {
+                 account.withdraw(amount);
+             }
+             else System.out.println("Insufficient balance");
+         }
+
+     }
+ }
 
 
